fece54d88a925c30aa56b33126447e169953b488
amaanpatel-dev <amaanweb32@gmail.com>
2024-12-31 18:11:48 +0530

    add design part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["Roboto Mono", "monospace"], // Add Roboto Mono to the "mono" font stack
+      },
+      colors: {
+        background: "#C36868", // Custom background color
+        primary: "#B3BCBB", // Custom primary color
+        button: "#323232", // Custom button color
+        btn2: "#96B8B8",
+      },
+    },
   },
   plugins: [],
-}
-
+};
